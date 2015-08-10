d7c9841fecaa775f673549218d24be28f731000e
Shannon Mulloy & Rachael Berecka <student@epicodus.com>
2015-08-10 16:15:38 -0700

    return true if sought key exists

--- lib/hash_class.rb
+++ lib/hash_class.rb
@@ -1,19 +1,28 @@
 class MyHash
   define_method(:initialize) do
     @array = []
-
   end
   define_method(:myStore) do |key, value|
     @key = key
     @value = value
     @array.push([@key, @value])
-
   end
   define_method(:myFetch) do |find_key|
     @array.each() do |sub_array|
       if sub_array[0] == find_key
         return sub_array[1]
       end
+    end
+  end
+  define_method(:key_exist?) do |find_it|
+    new_array = []
+    @array.each() do |sub_array|
+      new_array.push(sub_array[0])
+    end
+    if new_array.include?(find_it)
+      true
+    else
+      false
     end
   end
 end
--- spec/hash_class_spec.rb
+++ spec/hash_class_spec.rb
@@ -9,4 +9,11 @@ describe(MyHash) do
       expect(test_hash.myFetch("kitten")).to(eq("cute"))
     end
   end
+  describe("#key_exist?") do
+    it('checks if a hash has a specific key') do
+      test_hash = MyHash.new()
+      test_hash.myStore("monkeys", "crazy")
+      expect(test_hash.key_exist?("monkeys")).to(eq(true))
+    end
+  end
 end
