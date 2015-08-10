4ae55d499d1977406d982de80b3d984751d63985
Shannon Mulloy & Rachael Berecka <student@epicodus.com>
2015-08-10 15:46:27 -0700

    add my store method

--- lib/hash_class.rb
+++ lib/hash_class.rb
@@ -1,9 +1,13 @@
 class MyHash
-  define_method(:initialize) do |key, value|
+  define_method(:initialize) do
     @array = []
+
+  end
+  define_method(:myStore) do |key, value|
     @key = key
     @value = value
     @array.push([@key, @value])
+
   end
   define_method(:myFetch) do |find_key|
     @array.each() do |sub_array|
